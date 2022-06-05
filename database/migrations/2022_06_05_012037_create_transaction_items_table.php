686fd9d6c846f824a30617404119b73df7e61f8c
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-06-05 08:29:30 +0700

    feat: create model & table category & transaction

---
+++ database/migrations/2022_06_05_012037_create_transaction_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name');
+            $table->unsignedInteger('qty');
+            $table->enum('unit', ['Kg', 'Ons']);
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
